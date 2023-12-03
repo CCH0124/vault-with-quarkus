3e2d15e209b9b53e72f876fb81aac230a64ac9c9
CCH0124 <cchong0124@gmail.com>
2023-12-03 11:43:47 +0800

    fix

--- secret-csi-vault/src/main/java/org/cch/CertificateResource.java
+++ secret-csi-vault/src/main/java/org/cch/CertificateResource.java
@@ -1,14 +1,20 @@
 package org.cch;
 
+import java.util.Map;
+
 import org.cch.dto.ClientDTO;
 import org.cch.service.ClinetCertificate;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.resteasy.reactive.RestForm;
 import org.jboss.resteasy.reactive.RestResponse.Status;
 import org.jboss.resteasy.reactive.multipart.FileUpload;
 
 import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 @Path("/")
@@ -16,6 +22,10 @@ public class CertificateResource {
 
     @Inject
     ClinetCertificate service;
+
+    @ConfigProperty(name = "greeting.message") 
+    String message;
+
 
     @POST
     public Response multipart( @RestForm("client") FileUpload file) {
@@ -26,5 +36,12 @@ public class CertificateResource {
         }
 
         return Response.status(Status.BAD_REQUEST).build();
+    }
+
+    @GET
+    @Path("/info")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response into() {
+        return Response.ok(Map.of("message", this.message)).build();
     }
 }
--- secret-csi-vault/src/main/java/org/cch/GreetingResource.java
+++ secret-csi-vault/src/main/java/org/cch/GreetingResource.java
@@ -1,9 +1,5 @@
 package org.cch;
 
-import java.util.Map;
-
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -12,20 +8,9 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
-    @ConfigProperty(name = "greeting.message") 
-    String message;
-
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from RESTEasy Reactive";
     }
-
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Map into() {
-        return Map.of("message", this.message);
-    }
-
-
 }
